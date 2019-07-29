1381ccf8f95d32c9c53b370bdd552039eeb649ec
strotend <strotend@gmail.com>
2019-07-29 10:26:52 +0900

    TodoInput onKeyDown 시 addTodo 실행

--- src/containers/Todos/TodoInput.tsx
+++ src/containers/Todos/TodoInput.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const TodoInput: React.FunctionComponent = () => (
-  <div className="header">
-    <h1>todos</h1>
-    <input
-      className="new-todo"
-      placeholder="What needs to be done?"
-      defaultValue=""
-    />
-  </div>
-);
+interface PropsType {
+  todos: TodoType[];
+  addTodo: (title: string) => void;
+}
+
+const TodoInput: React.FunctionComponent<PropsType> = props => {
+  const refInput = useRef<HTMLInputElement>(null);
+
+  const handleKeyDown: React.KeyboardEventHandler = event => {
+    if (refInput.current instanceof HTMLElement) {
+      if (event.key === "Enter") {
+        const title = refInput.current.value;
+        refInput.current.value = "";
+        props.addTodo(title);
+      }
+    }
+  };
+
+  return (
+    <div className="header">
+      <h1>todos</h1>
+      <input
+        ref={refInput}
+        className="new-todo"
+        placeholder="What needs to be done?"
+        defaultValue=""
+        onKeyDown={handleKeyDown}
+      />
+    </div>
+  );
+};
 
 export default TodoInput;
