736e562c7f9dc89ccc5eb5445601765ad39a3e0c
strotend <strotend@gmail.com>
2019-08-19 15:07:01 +0900

    TodoListItem title 수정기능 추가

--- src/containers/Todos/TodoListItem.tsx
+++ src/containers/Todos/TodoListItem.tsx
@@ -11,7 +11,18 @@ interface PropsType {
 }
 
 const TodoListItem: React.FunctionComponent<PropsType> = props => {
-  const handleChangeCompleted: React.ChangeEventHandler<
+  const [editing, setEditing] = useState<boolean>(false);
+  const [todoTitle, setTodoTitle] = useState<string>(props.todo.title);
+
+  const updateTodoTitle = () => {
+    if (todoTitle) {
+      props.updateTodo(props.todo.id, { title: todoTitle });
+    } else {
+      setTodoTitle(props.todo.title);
+    }
+  };
+
+  const handleChangeCompletedCheckbox: React.ChangeEventHandler<
     HTMLInputElement
   > = event => {
     if (event.target.checked) {
@@ -26,23 +37,48 @@ const TodoListItem: React.FunctionComponent<PropsType> = props => {
     }
   };
 
-  const handleClickDelete: React.MouseEventHandler = () => {
+  const handleDoubleClickLabel: React.MouseEventHandler = () => {
+    setEditing(true);
+  };
+
+  const handleClickDeleteButton: React.MouseEventHandler = () => {
     props.deleteTodo(props.todo.id);
   };
 
-  const handleClickStar: React.MouseEventHandler = event => {
+  const handleClickStarButton: React.MouseEventHandler = () => {
     props.updateTodo(props.todo.id, { starred: !props.todo.starred });
   };
 
+  const handleChangeEditingInput: React.ChangeEventHandler<
+    HTMLInputElement
+  > = event => {
+    setTodoTitle(event.target.value);
+  };
+  const handleBlurEditingInput: React.FocusEventHandler = () => {
+    updateTodoTitle();
+    setEditing(false);
+  };
+  const handleKeyPressEditingInput: React.KeyboardEventHandler = event => {
+    if (event.key === "Enter") {
+      updateTodoTitle();
+      setEditing(false);
+    }
+  };
+
+  const classNames = [
+    props.todo.completed ? "completed" : "",
+    editing ? "editing" : ""
+  ];
+
   return (
-    <li className={props.todo.completed ? "completed" : undefined}>
+    <li className={classNames.join(" ")}>
       <div className="view">
         <input
           className="toggle"
           style={{ height: "40px" }}
           type="checkbox"
           checked={props.todo.completed}
-          onChange={handleChangeCompleted}
+          onChange={handleChangeCompletedCheckbox}
         />
         <label
           style={{
@@ -51,22 +87,32 @@ const TodoListItem: React.FunctionComponent<PropsType> = props => {
               : "url(data:image/svg+xml;charset=utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23ededed%22%20stroke-width%3D%223%22/%3E%3C/svg%3E)",
             backgroundSize: "40px 40px"
           }}
+          onDoubleClick={handleDoubleClickLabel}
         >
           {props.todo.title}
         </label>
         {!props.todo.completed && (
           <button
             className={props.todo.starred ? "star" : "unstar"}
-            onClick={handleClickStar}
+            onClick={handleClickStarButton}
           />
         )}
         <button
           className="destroy"
           style={{ cursor: "pointer" }}
-          onClick={handleClickDelete}
+          onClick={handleClickDeleteButton}
         />
       </div>
-      <input className="edit" value={props.todo.title} readOnly />
+      {editing && (
+        <input
+          autoFocus
+          className="edit"
+          value={todoTitle}
+          onBlur={handleBlurEditingInput}
+          onChange={handleChangeEditingInput}
+          onKeyPress={handleKeyPressEditingInput}
+        />
+      )}
     </li>
   );
 };
